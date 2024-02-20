8868408d369337c72c59a8f9fcd7b2658ebb9be4
Alexis212 <alexissalcedo212@gmail.com>
2024-02-20 00:20:45 -0600

    Feature: Lexer almost completed.

--- lexer.py
+++ lexer.py
@@ -6,6 +6,11 @@ import sys
 class Lexer:
     """Divide el texto en tokens."""
     ERR = -1
+
+    ER1 = -1
+    ER2 = -2
+    ER3 = -2
+
     ACP = 99
     SIM = [4, 5]
     COM = 7
@@ -26,26 +31,27 @@ class Lexer:
     #     0    1    2    3    4    5    6    7    8    9    10   11   12   13   14   15
     # TODO: Eliminar todos los estados de error?
     transiciones = [
-        [ 19,   0,   0,   1,   4,  17,  18,   6,   6,   8, ERR,  11,  13,  15,  16,  18],  # 0
-        [ACP, ACP, ACP,   1, ACP, ERR,   2, ACP, ACP, ACP, ERR, ACP, ACP, ACP, ACP, ACP],  # 1 Entero
-        [ERR, ERR, ERR,   3, ERR, ERR, ERR, ERR, ERR, ERR, ERR, ERR, ERR, ERR, ERR, ERR],  # 2
-        [ACP, ACP, ACP,   3, ACP, ACP, ERR, ACP, ACP, ACP, ERR, ACP, ACP, ACP, ACP, ACP],  # 3 Decimal
-        [ACP, ACP, ACP,   4,   4,   5, ACP, ACP, ACP, ACP, ERR, ACP, ACP, ACP, ACP, ACP],  # 4 Simbolo
-        [ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ERR, ACP, ACP, ACP, ACP, ACP],  # 5 Simbolo!
-        [ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP,   7, ACP, ERR, ACP, ACP, ACP, ACP, ACP],  # 6 Operador Matematico
+        [ 19,   0,   0,   1,   4,  17,  20,   6,   6,   8,  19,  11,  13,  15,  16,  18],  # 0
+        [ACP, ACP, ACP,   1, ACP, ACP,   2, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP],  # 1 Entero
+        [ER1, ER1, ER1,   3, ER1, ER1, ER1, ER1, ER1, ER1, ER1, ER1, ER1, ER1, ER1, ER1],  # 2
+        [ACP, ACP, ACP,   3, ACP, ACP, ER1, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP],  # 3 Decimal
+        [ACP, ACP, ACP,   4,   4,   5, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP],  # 4 Simbolo
+        [ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP],  # 5 Simbolo!
+        [ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP,   7, ACP, ACP, ACP, ACP, ACP, ACP, ACP],  # 6 Operador Matematico
         [  7,   7, ACP,   7,   7,   7,   7,   7,   7,   7,   7,   7,   7,   7,   7,   7],  # 7 Comentario //
         [  8,   8,   8,   8,   8,   8,   8,   8,   8,   9,  10,   8,   8,   8,   8,   8],  # 8
-        [ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ERR, ACP, ACP, ACP, ACP, ACP],  # 9 Cadena de Texto
+        [ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP],  # 9 Cadena de Texto
         [  8,   8,   8,   8,   8,   8,   8,   8,   8,   8,   8,   8,   8,   8,   8,   8],  # 10
-        [ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ERR, ACP,  12, ACP, ACP, ACP],  # 11 Operador Relacional <>
-        [ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ERR, ACP, ACP, ACP, ACP, ACP],  # 12 Operador Relacional !=
-        [ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ERR, ACP,  12, ACP, ACP, ACP],  # 13 Operador de Asignación
-        [ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ERR, ACP, ACP, ACP, ACP, ACP],  # 14 Operador Lógico
-        [ERR, ERR, ERR, ERR, ERR, ERR, ERR, ERR, ERR, ERR, ERR, ERR, ERR,  14, ERR, ERR],  # 15 |
-        [ERR, ERR, ERR, ERR, ERR, ERR, ERR, ERR, ERR, ERR, ERR, ERR, ERR, ERR,  14, ERR],  # 16 &
-        [ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ERR, ACP,  12, ACP, ACP, ACP],  # 17 Operador Lógico !
-        [ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ERR, ACP, ACP, ACP, ACP, ACP],  # 18 Delimitador
-        [ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ERR, ACP, ACP, ACP, ACP, ACP]   # 19 Simbolo Especial
+        [ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP,  12, ACP, ACP, ACP],  # 11 Operador Relacional <>
+        [ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP],  # 12 Operador Relacional !=
+        [ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP,  12, ACP, ACP, ACP],  # 13 Operador de Asignación
+        [ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP],  # 14 Operador Lógico
+        [ER2, ER2, ER2, ER2, ER2, ER2, ER2, ER2, ER2, ER2, ACP, ER2, ER2,  14, ER2, ER2],  # 15 |
+        [ER2, ER2, ER2, ER2, ER2, ER2, ER2, ER2, ER2, ER2, ACP, ER2, ER2, ER2,  14, ER2],  # 16 &
+        [ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP,  12, ACP, ACP, ACP],  # 17 Operador Lógico !
+        [ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP],  # 18 Delimitador
+        [ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP],  # 19 Simbolo Especial
+        [ACP, ACP, ACP, ER1, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP, ACP]   # 20 Delimitador .
     ]
 
     columnas = [
@@ -68,14 +74,14 @@ class Lexer:
 
     default = 0
     tipos = {
-       101: 'Palabra Reservada',
-       102: 'Booleano',
-       103: 'Identificador',
+        # Errores
+        -1: 'decimal incompleto', 
+        -2: 'operador logico incompleto', 
+        # -3: 'backlash outside of string', 
 
+        # Tipos
          1: 'Entero',
          3: 'Decimal',
-       # 4: 'Simbolo',
-       # 5: 'Simbolo',
          6: 'Operador Matematico',
          7: 'Comentario',
          9: 'Cadena de texto',
@@ -85,7 +91,13 @@ class Lexer:
         14: 'Operador Lógico',
         17: 'Operador Lógico',
         18: 'Delimitador',
-        19: 'Simbolo Especial'
+        19: 'Simbolo Especial',
+        20: 'Delimitador',
+
+        # Simbolos
+       101: 'Palabra Reservada',
+       102: 'Booleano',
+       103: 'Identificador'
     }
 
     def __call__(self, entrada):
@@ -108,7 +120,7 @@ class Lexer:
                     # output.append((tipo, "###"))
                     lexema = ""
                     estado = 0
-                   
+
                 else:
                     if estado_anterior in self.SIM:
                         estado_anterior = 101 if lexema in self.reservadas else 102 if lexema in self.booleanos else 103
@@ -121,18 +133,37 @@ class Lexer:
                     self.columna_archivo -= 1
                     estado = 0
 
-            elif estado == self.ERR:
+            # All errors are negative 
+            elif estado < 0:
                 lexema += token
                 output.append(('ERR_0', lexema))
-                print(f"[{self.linea_archivo}][{self.columna_archivo}] Error lexico, constante decimal mal definida: {lexema}")
-                # return output
+                tipo = self.tipos.get(estado, 'ERR_1')
+                print(f"[{self.linea_archivo}][{self.columna_archivo}] Error lexico, {tipo}: {lexema}")
 
                 lexema = ""
                 estado = 0
+                # idx -= 1
+                # self.columna_archivo -= 1
+
+                # return output
 
             else:
-                if (not token in [' ', '\n', '\t'] and estado != self.COM) or estado == self.STR:
+                if estado_anterior == self.STR:
+                    if token != '\\':
+                        lexema += token
+
+                elif estado_anterior == 10:
+                    if token == 'n':
+                        token = '\n'
+
+                    elif token == 't':
+                        token = '\t'
+
                     lexema += token
+
+                else:
+                    if estado != self.COM and token not in [' ', '\n', '\t']:
+                        lexema += token
 
             # Contador de lineas
             if token == '\n':
@@ -144,6 +175,10 @@ class Lexer:
             estado_anterior = estado
 
         # TODO: Procesar ultimo token?
+        if estado_anterior == self.STR:
+            output.append(('ERR_0', lexema))
+            print(f"[{self.linea_archivo}][{self.columna_archivo}] Error lexico, cadena nunca finalizada: {lexema}")
+
         return output
 
     def _obtener_columna(self, c):
